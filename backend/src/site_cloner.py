39a5e5c85690135441360ad25e68bca40e04ab54
Ashish Belwase <belwasetech@Ashishs-MacBook-Pro.local>
2020-09-14 11:13:09 +0545

    fix frozing issue at last of crawling

--- backend/src/site_cloner.py
+++ backend/src/site_cloner.py
@@ -1,11 +1,24 @@
-from pywebcopy import save_website
 from src.config import Config
+
+from pywebcopy import WebPage  # save_website
+from pywebcopy import config
 
 
 class SiteCloner:
     @staticmethod
-    def clone(url):
-        name = url.split("//")[1]
-        kwargs = {"project_name": name}
+    def scrape(url, folder, timeout=1):
+        config.setup_config(url, folder)
 
-        save_website(url=url, project_folder=Config.static_path, **kwargs)
+        wp = WebPage()
+        wp.get(url)
+
+        wp.save_complete()
+        for t in wp._threads:
+            if t.is_alive():
+                t.join(timeout)
+
+        return wp.file_path
+
+    @staticmethod
+    def clone(url):
+        SiteCloner.scrape(url=url, folder=Config.static_path)
